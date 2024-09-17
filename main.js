e2090598579b4bdbf996bfdf6086485f776ab4c1
Darrell <176396718+buheracoupe@users.noreply.github.com>
2024-09-17 18:30:36 +0200

    add updateTimer function to display time remaining in realtime

--- main.js
+++ main.js
@@ -1,2 +1,43 @@
 const clock = document.querySelector(".time");
-const startBtn = document.querySelector(".start-pause")
+const startBtn = document.querySelector("#start-pause")
+const timeBtns = document.querySelectorAll("[data-time]")
+const endTime = document.querySelector(".time-notice")
+let timeSelected;
+
+// udpate selected btn
+timeBtns.forEach((btn) => {
+    btn.addEventListener("click", ()=>{
+        timeBtns.forEach((btn) =>{
+            btn.classList.remove("active")
+        })
+        btn.classList.add("active")
+        console.log(btn.dataset.time)
+       const programTime = parseInt(btn.dataset.time)
+       clock.textContent = `${programTime}:00`
+       timeSelected = programTime * 60//time selected in seconds
+
+    })
+})
+
+
+function handleStart(){
+    const now = Date.now();
+    const then = timeSelected*1000 + now;
+    const timeOver = new Date(then);
+    const timeHours = timeOver.getHours();
+    const timeMinutes = timeOver.getMinutes();
+
+    endTime.textContent = `Your interval ends at ${timeHours}:${timeMinutes}`
+    updateTimer()
+}
+
+//
+function updateTimer(){
+    
+const minutes = timeSelected/60;
+const seconds = timeSelected % 60;
+}
+
+
+
+startBtn.addEventListener("click", handleStart)
